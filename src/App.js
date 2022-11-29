65c762593ae883e8f0a4df3a82f273ad2980ac80
Khalid-Hasan96 <khbappy66@gmail.com>
2022-11-29 18:54:29 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Router/Router';
 
 function App() {
   return (
     <div>
-      <h1 className='text-5xl'>Testing font</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
